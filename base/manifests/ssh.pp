95ba0ba5f2dd234195c4066507fb0938933bc2bb
arjunp <arjun>
2017-02-05 17:08:55 +0000

    Selectors Statement

--- base/manifests/ssh.pp
+++ base/manifests/ssh.pp
@@ -1,8 +1,14 @@
 class base::ssh {
-  case $osfamily {
-        'Redhat' : { $ssh_name = 'sshd' }
-        'Debian' : { $ssh_name = 'ssh' }
-        default  : { fail('OS not suported by puppet module ssh') }
+  #  case $osfamily {
+  #     'Redhat' : { $ssh_name = 'sshd' }
+  #     'Debian' : { $ssh_name = 'ssh' }
+  #     default  : { fail('OS not suported by puppet module ssh') }
+  #}
+
+  $ssh_name = $osfamily ? {
+              'RedHat' => 'sshd',
+              'Debian' => 'ssh',
+              default  => 'value',
   }
 
   package { 'openssh-package':
