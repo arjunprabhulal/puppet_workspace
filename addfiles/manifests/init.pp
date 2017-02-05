8a799f17be490d67aac9f18b9ba54582e4533754
arjunp <arjun>
2017-02-05 15:47:05 +0000

    Adding addfiles modules for file creation

---
+++ addfiles/manifests/init.pp
@@ -0,0 +1,54 @@
+# == Class: files
+#
+# Full description of class files here.
+#
+# === Parameters
+#
+# Document parameters here.
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# === Variables
+#
+# Here you should define a list of variables that this module would require.
+#
+# [*sample_variable*]
+#   Explanation of how this variable affects the funtion of this class and if
+#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#   External Node Classifier as a comma separated list of hostnames." (Note,
+#   global variables should be avoided in favor of class parameters as
+#   of Puppet 2.6.)
+#
+# === Examples
+#
+#  class { addfiles:
+#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#  }
+#
+# === Authors
+#
+# Author Name <author@domain.com>
+#
+# === Copyright
+#
+# Copyright 2017 Your name here, unless otherwise noted.
+#
+class addfiles {
+  file { '/root/motd':
+        ensure  => present,
+        source  => 'puppet:///modules/addfiles/motd',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+  }
+  
+  file { '/etc/motd':
+        ensure => link,
+        target => '/root/motd',
+  }
+  
+
+
+}
