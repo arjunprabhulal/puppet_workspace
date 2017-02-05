a75d35432383ab688ddaeb6ff916ff29d41ae759
arjunp <arjun>
2017-02-05 19:32:19 +0000

    Addin Metaparameters - exec and schedule

--- base/manifests/init.pp
+++ base/manifests/init.pp
@@ -41,8 +41,21 @@ class base {
         'Debian' => 'dnsutils',
     }
 
+    $systemupdate = $osfamily ? {
+        'Redhat' => '/usr/bin/yum update -y',
+        'Debian' => '/usr/bin/apt-get upgrade -y',
+    }
+
     package { ['tree',$dnsutil]:
               ensure => present,
+    }
+   
+    schedule { 'system-daily':
+                period => daily,
+                range  => '00:00 - 01:00',
+    }
+    exec { $systemupdate: 
+            schedule => 'system-daily',
     }
 
 }
