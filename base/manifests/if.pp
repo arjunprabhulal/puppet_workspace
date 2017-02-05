790cf42e59d6ba4a2ff0ff0a4b32c02965918be6
arjunp <arjun>
2017-02-05 17:20:17 +0000

    Regular Expression If Statement

---
+++ base/manifests/if.pp
@@ -0,0 +1,7 @@
+class base::if {
+  if $::hostname =~ /^arjunp(\d+)/ {
+         notice ("You have arrived at server $0 ")
+  }
+
+}
+
--- base/manifests/ssh.pp
+++ base/manifests/ssh.pp
@@ -1,15 +1,10 @@
 class base::ssh {
-  #  case $osfamily {
-  #     'Redhat' : { $ssh_name = 'sshd' }
-  #     'Debian' : { $ssh_name = 'ssh' }
-  #     default  : { fail('OS not suported by puppet module ssh') }
-  #}
-
-  $ssh_name = $osfamily ? {
-              'RedHat' => 'sshd',
-              'Debian' => 'ssh',
-              default  => 'value',
+  case $osfamily {
+       'Redhat' : { $ssh_name = 'sshd' }
+       'Debian' : { $ssh_name = 'ssh' }
+       default  : { fail('OS not suported by puppet module ssh') }
   }
+
 
   package { 'openssh-package':
           name   => 'openssh',
